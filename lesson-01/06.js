699ee2f1f64083fe5e69c723b27f708d36116db7
stas <121128372+Stasik77@users.noreply.github.com>
2023-11-11 21:30:40 +0300

    tst

--- lesson-01/06.js
+++ lesson-01/06.js
@@ -1,10 +1,11 @@
 let str = "zzzzyzz";
-if (str.includes('y')){
-    console.log("нашел");
-}
-    else{
 
-        console.log("заблудился");
+console.log(str.includes('y') ?"нашел" :"заблудился");
+    
+ 
 
 
-    }
+     
+
+
+    
