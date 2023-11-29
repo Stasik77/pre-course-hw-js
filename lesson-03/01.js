7296c07ddfbc794e40a416098edf66696da26fdf
stas <121128372+Stasik77@users.noreply.github.com>
2023-11-29 22:24:01 +0300

    w

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,9 +1,8 @@
 // code
-// code
 ​const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-    return colors.filter((colors)=> colors ==="черный" || colors ==="красный" ||  colors ==="желтый").join('-')
+    return colors.filter(colors=> colors ==="черный" || colors ==="красный" ||  colors ==="желтый").join('-')
 }
 
 console.log(createColorString()); 
